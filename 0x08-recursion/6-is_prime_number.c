ecaedb1736e4eba7d7ccead2e21a9d22fa41843c
ningi265 <ningintonga@gmail.com>
2023-10-25 08:27:11 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -20,12 +20,12 @@ int is_prime_number(int n)
  * @n: number to evaluate
  * @i: iterator
  *
- * Return: 1 if is prime, 0 if not
+ * Return: 1 if n is prime, 0 if not
  */
 int actual_prime(int n, int i)
 {
 	if (i == 1)
-		return (i);
+		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
 	return (actual_prime(n, i - 1));
